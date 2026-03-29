6da8b02e6adc632ab7f74bf9235d4f1b51d8fb7f
Mu <dangbui623@gmail.com>
2026-03-29 09:07:15 +0700

    feat: implement account management system with email verification and RabbitMQ event handling

---
+++ src/main/resources/db/migration/V5__Drop_UpdatedByAccount.sql
@@ -0,0 +1,14 @@
+-- Kiểm tra nếu cột tồn tại thì mới xóa để tránh lỗi khi chạy lại
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM information_schema.columns
+               WHERE table_name='' AND column_name='avatar_url') THEN
+
+        -- 1. Nếu cần, hãy đảm bảo dữ liệu từ avatar_url đã được backup sang avatar_path
+UPDATE accounts SET avatar_path = avatar_url WHERE avatar_path IS NULL;
+
+-- 2. Xóa cột thừa
+ALTER TABLE accounts DROP COLUMN avatar_url;
+
+END IF;
+END $$;
